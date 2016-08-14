d4d1e7f8f759f5b4b2429ab3d46bac3db2c54b7c
lynnerw <larwitter@gmail.com>
2016-08-13 23:56:35 -0400

    added a little styling

--- js/bartender.js
+++ js/bartender.js
@@ -32,6 +32,8 @@ var wineChoice.prototype.wineWeight = function(weight) {
 
 var wineColor = "";
 var wineFlavor = "";
+var wineWeight = "";
+
 $('.wine-flavor').hide();
 $('.wine-weight').hide();
 
@@ -40,36 +42,36 @@ function getWineFlavor(wineColor) {
     $('.wine-flavor').click( function(e){
         e.preventDefault();
     var wineFlavor = $('input[name=wineFlavor]:checked').val();
-    console.log('user response is' + wineColor + "and" + wineFlavor);
+    console.log('User selection is ' + wineColor + ' and ' + wineFlavor);
 
-    if (wineColor == "red") {
+    if (wineColor == 'red') {
 
-        if (wineFlavor == "sweet") {
-            $('.wine-weight').prepend("<p>Do you like your sweet red wine full bodied or light?</p>");
+        if (wineFlavor == 'sweet') {
+            $('.wine-weight').prepend("<p>Do you like your sweet red wine to resemble a substantial desert wine or to be more light and fruity?</p>");
             $('.wine-weight').show();
         } else {
-            $('.wine-weight').prepend("<p>Do you like your dry red wine full bodied or light?</p>");
+            $('.wine-weight').prepend("<p>Do you like your dry red wine to be complex and full-bodied or light, with a crisp finish?</p>");
             $('.wine-weight').show();
           }
-      }  else {//wineColor is "white"
-        if (wineFlavor == "sweet") {
-            $('.wine-weight').prepend("<p>Do you like your sweet white wine full bodied or light?</p>");
+      }  else {            // wineColor must be 'white'
+
+        if (wineFlavor == 'sweet') {
+            $('.wine-weight').prepend("<p>Do you like your sweet white wine to be a little buttery or more on the fruity side?</p>");
             $('.wine-weight').show();
         } else {
-            $('.wine-weight').prepend("<p>Do you like your dry white wine full bodied or light?</p>");
+            $('.wine-weight').prepend("<p>Do you like your dry white wine to be complex and full-bodied or light, with a crisp finish?</p>");
             $('.wine-weight').show();
           };
-      };
-      // end of if - else
+      };  // end of if (wineColor)
 
     // getWineWeight();
     });
 }
 
-var wineChoice = function(wineType) {
+/*var wineChoice = function(wineType) {
     this.wineType = wineType;
     console.log("Wine type is " + wineType);
-}
+}*/
 
 $(document).ready( function() {
 
